de868794002cb793d089b9cc62fee4dc55b25d0b
Twana <tuwana.ibrahim99@gmail.com>
2024-03-23 15:49:43 +0300

    adding theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,10 @@
+import { ScreenType } from "@/hooks/useWindowSize";
+
+export const themeToggleIconSize: { [S in ScreenType]: number } = {
+  mobile: 40,
+  sm: 40,
+  md: 40,
+  lg: 18,
+  xl: 18,
+  "2xl": 18,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,19 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        brand: "rgb(var(--color-brand) / <alpha-value>)",
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        ternary: "rgb(var(--color-ternary) / <alpha-value>)",
       },
     },
   },
