dd5d5629a2f347bad63c2ce4d64d488116206137
Twana <tuwana.ibrahim99@gmail.com>
2024-05-15 18:47:37 +0300

    Skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Home from "@/components/Sections/Home";
 import Projects from "@/components/Sections/Projects";
 import SectionDivider from "@/components/SectionDivider";
+import Skills from "@/components/Sections/Skills";
 
 export default function Main() {
   return (
@@ -8,6 +9,8 @@ export default function Main() {
       <Home />
       <SectionDivider />
       <Projects />
+      <SectionDivider />
+      <Skills />
     </main>
   );
 }
